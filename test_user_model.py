c5d12ac2d5e619903363e7fc4835f4afcf1184e8
NicholasK <nicholask1@outlook.com>
2022-02-21 08:17:00 -0500

    Made tests for the User model

--- test_user_model.py
+++ test_user_model.py
@@ -41,18 +41,72 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
-    def test_user_model(self):
-        """Does basic model work?"""
-
-        u = User(
+        self.u = User(
             email="test@test.com",
             username="testuser",
             password="HASHED_PASSWORD"
         )
 
-        db.session.add(u)
+        self.u2 = User(
+            email="test2@test2.com",
+            username="testuser2",
+            password="HASHED_PASSWORD2"
+        )
+
+        db.session.add_all([self.u, self.u2])
         db.session.commit()
 
+
+    def test_user_model(self):
+        """Does basic model work?"""
+
         # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(self.u.messages), 0)
+        self.assertEqual(len(self.u.followers), 0)
+
+    def test_repr(self):
+        """Tests the __repr__ method"""
+
+        self.assertEqual(repr(self.u), f"<User #{self.u.id}: {self.u.username}, {self.u.email}>")
+
+    def test_is_following(self):
+        """Tests if u2 is followed by u"""
+        
+        self.u.following.append(self.u2)
+
+        self.assertIn(self.u2, self.u.following)
+
+    def test_is_not_following(self):
+        """Tests if u2 is not following u"""
+
+        self.assertNotIn(self.u2, self.u.following)
+
+    def test_is_followed_by(self):
+        """Tests if u2 is following u"""
+
+        self.u.followers.append(self.u2)
+
+        self.assertIn(self.u2, self.u.followers)
+
+    def test_is_not_followed_by(self):
+
+        self.assertNotIn(self.u2, self.u.followers)
+
+    def test_user_authenticate(self):
+
+        u3 = User(
+            email="test2@test3.com",
+            username="testuser3",
+            password="HASHED_PASSWORD3"
+        )
+        db.session.commit()
+
+        user = User.signup(u3.username, u3.email, u3.password, u3.image_url)
+        auth_user_username = User.authenticate("mike", user.password)
+        auth_user_password = User.authenticate(user.username, "WRONG_PASS")
+        auth_user_complete = User.authenticate(u3.username, u3.password)
+
+        self.assertNotEqual(user, auth_user_username)
+        self.assertIsNot(user, auth_user_password)
+        self.assertIs(user, auth_user_complete)
+
